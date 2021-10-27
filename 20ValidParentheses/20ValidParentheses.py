86c66a83273f3cfc7d173c3273b7e0c521d80efd
HaoyuCreate <hf945@nyu.edu>
2021-10-27 18:49:39 -0400

    problem 20

--- 20ValidParentheses/20ValidParentheses.py
+++ 20ValidParentheses/20ValidParentheses.py
@@ -1,35 +1,26 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        # stack for "("
-        stack1 = []
-        # stack for "["
-        stack2 = []
-        # stack for "{"
-        stack3 = []
+    def isValid2(self,s):
+        temp = [] #initial the array to avoid index error
 
-        for chr in list(s):
-            if chr == '(' or chr == ')':
-                top = stack1.pop() if len(stack1)>0 else 0
-                val = -1 if chr == '(' else 1
-                if top + val != 0:
-                    stack1.extend([top,val])
+        for chr in s:
+            val = temp[-1] if temp else '#'
+            if self.Matched(val,chr):
+                temp.pop()
+            else:
+                temp.append(chr)
+        return (len(temp)==0)
 
-            if chr == '[' or chr == ']':
-                top = stack2.pop() if len(stack2)>0 else 0
-                val = -1 if chr == '[' else 1
-                if top + val != 0:
-                    stack2.extend([top,val])
-
-            if chr == '{' or chr == '}':
-                top = stack3.pop() if len(stack3) > 0 else 0
-                val = -1 if chr == '[' else 1
-                if top + val != 0:
-                    stack3.extend([top, val])
-
-        return (sum(stack1) == 0 and sum(stack2) == 0 and sum(stack3) == 0)
-
+    def Matched(self,s1,s2):
+        matched = False
+        if s1 == '(' and s2 ==')':
+            matched = True
+        if s1 == '[' and s2 ==']':
+            matched = True
+        if s1 == '{' and s2 =='}':
+            matched = True
+        return matched
 
 if __name__ == '__main__':
     solution = Solution()
-    s = "([)]"
-    print(solution.isValid(s))
+    s = "{[]}"
+    print(solution.isValid2(s))
