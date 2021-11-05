461f9120a5430808d9f3df4994a81027df3ba31f
HaoyuCreate <hf945@nyu.edu>
2021-11-05 18:03:13 -0400

    problem 217 python/C++

---
+++ 217ContainsDuplicate/217ContainDuplicate.cpp
@@ -0,0 +1,16 @@
+//
+// Created by vincent on 11/2/21.
+//
+
+#include "217ContainDuplicate.h"
+
+class Solution{
+public:
+    bool containsDuplicate(vector<int>& nums)
+};
+
+void main()
+{
+    Solution solution;
+
+}
