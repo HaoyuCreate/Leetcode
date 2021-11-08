a4851a33c86c1de2e572efb4f3140364828142ca
HaoyuCreate <hf945@nyu.edu>
2021-11-08 17:03:49 -0500

    problem 83

---
+++ 83RemoveDuplicatesfromSortedList/83RemoveDuplicatesfromSortedList.py
@@ -0,0 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head) ->:
+        curr = head
+        while curr:
+            if curr.next and (curr.next.val == curr.val):
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
