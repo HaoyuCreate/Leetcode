512970b77d75bfc54127c5333e133bd5a65c15cf
HaoyuCreate <hf945@nyu.edu>
2021-11-09 01:28:03 -0500

    problem 141 & 142

--- 141LinkedListCycle/141LinkedListCycle.py
+++ 141LinkedListCycle/141LinkedListCycle.py
@@ -1,27 +1,15 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        #         slow = head
-        #         if head:
-        #             fast = head.next
-        #         else:
-        #             return False
-        #         curr = head
-
-        #         while slow != fast:
-        #             if not fast or not fast.next:
-        #                 return False
-        #             slow = slow.next
-        #             fast = fast.next.next
-        #         return True
-
-        slow = head
-        fast = head
+    def hasCycle(self, head) -> bool:
+        #1. check if head is None
+        if not head:
+            return False
+        curr = slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
@@ -29,8 +17,48 @@ class Solution:
                 return True
         return False
 
-        # return the start of the loop
-        # while slow != curr:
-        #     slow = slow.next
-        #     curr = curr.next
-        # return curr
+    def detectCycle(self, head: ListNode) -> ListNode:
+        start = None
+        # 1. check if head is None
+        if not head:
+            return start
+        curr = slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+
+        # 2. check is the iteration stops by finding an end or finding a loop
+        if not fast or not fast.next:
+            return start
+
+        # 3. return the start of the loop
+        while slow != curr:
+            slow = slow.next
+            curr = curr.next
+        return curr
+
+    def detectCycle2(self, head: ListNode) -> ListNode:
+        '''A better grammer by while{}else{}'''
+        start = None
+        # 1. check if head is None
+        if not head:
+            return start
+        curr = slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            # 2. check is the iteration stops by finding an end or finding a loop
+            return start
+
+        # 3. return the start of the loop
+        while slow != curr:
+            slow = slow.next
+            curr = curr.next
+        return curr
