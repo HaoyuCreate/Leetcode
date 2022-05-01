9f248dbb7bd9513c0adb9e4c54eb86f0b279a4f2
HaoyuCreate <hf945@nyu.edu>
2022-04-30 23:39:44 -0400

    problem 740

---
+++ 740DeleteandEarn/740DeleteandEarn.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calPoints(self, key):
+        return self.valueDict[key] * key
+
+    def deleteAndEarn(self, nums) -> int:
+        from collections import Counter
+
+        self.valueDict = dict(Counter(nums))
+
+        keyList = list(self.valueDict.keys())
+        keyList.sort()
+
+        # dp[i] = max(dp[i-1],dp[i-2]+calPoints[keyList[i]]) refers to the max value at key=keyList[i]
+
+        if len(keyList) == 1:
+            return self.calPoints(keyList[0])
+
+        if len(keyList) == 2:
+            return max(self.calPoints(keyList[0]), self.calPoints(keyList[1])) if keyList[1] == keyList[0] + 1 else (
+                        self.calPoints(keyList[1]) + self.calPoints(keyList[0]))
+
+        print(keyList)
+        dp = [0] * 3
+        dp[0] = self.calPoints(keyList[0])
+        dp[1] = max(dp[0], self.calPoints(keyList[1])) if keyList[1] == keyList[0] + 1 else (
+                    self.calPoints(keyList[1]) + dp[0])
+
+        for i in range(2, len(keyList)):
+            dp[2] = max(dp[1], dp[0] + self.calPoints(keyList[i])) if keyList[i] == (keyList[i - 1] + 1) else (
+                        dp[1] + self.calPoints(keyList[i]))
+            dp[0] = dp[1]
+            dp[1] = dp[2]
+
+        return dp[2]
